db946348d9d118d10388366e51c878d20dd2ece6
OtterWhisperer <sundowneffect@gmail.com>
2021-01-02 02:44:10 +0100

    add search for keywords in a given text

--- projects/library-template/src/lib/library-template.component.ts
+++ projects/library-template/src/lib/library-template.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'lib-library-template',
   template: `
-    <p>
-      library-template works!
-    </p>
+    <input type="test" (keydown.enter)="markText($event.target.value)">
+    <div #content [hidden]="true">
+      <ng-content></ng-content>
+    </div>
+
+    <div [innerHTML]="controlledContent"></div>
   `,
-  styles: [
-  ]
+  styles: [`.mark {background-color: green}`],
+  encapsulation: ViewEncapsulation.None
 })
-export class LibraryTemplateComponent implements OnInit {
+export class LibraryTemplateComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('content') content: ElementRef;
 
-  ngOnInit(): void {
+  originalContent: string;
+  controlledContent: string;
+
+  ngAfterViewInit(): void {
+    this.controlledContent = this.originalContent = this.content.nativeElement.textContent;
+  }
+
+  markText(value: any) {
+    this.controlledContent = this.originalContent;
+    this.controlledContent = this.originalContent.replace(
+      new RegExp(value, 'g'),
+        `<span class="mark">${value}</span>`
+      );
+    console.log(value);
   }
 
 }
